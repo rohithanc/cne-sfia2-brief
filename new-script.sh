9a5d89720bf7ed1bb45430dabb18d5c838bc7d57
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-20 14:54:52 +0000

    changing file

--- new-script.sh
+++ new-script.sh
@@ -1,7 +1,7 @@
 ssh -tt -i /home/jenkins/.ssh/id_rsa ubuntu@52.215.78.226 << EOF
 sudo rm -r cne-sfia2-brief
 git clone http://github.com/rohithanc/cne-sfia2-brief
-cd cne-sfia2-brief/database
+cd cne-sfia2-brief
 mysql -h cne-testdb.cjhtseh9p1qi.eu-west-1.rds.amazonaws.com -P 3306 -u rohithanc -pPassword123 < Create.sql
 mysql -h cne-productiondb.cjhtseh9p1qi.eu-west-1.rds.amazonaws.com -P 3306 -u rohithanc -pPassword123 < Create.sql
 sudo docker-compose up -d
