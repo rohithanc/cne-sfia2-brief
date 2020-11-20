3df97e796a99a3fa7268b12f62715c1f4791cd10
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-20 22:41:14 +0000

    build images

---
+++ docker-images.sh
@@ -0,0 +1,8 @@
+sudo docker login -u rohithan9615 -p
+cd frontend
+sudo docker build -t rohithan9615/frontend
+cd ..
+cd backend
+sudo docker build -t rohithan9615/backend
+sudo docker push rohithan9615/frontend
+sudo docker push rohithan9615/backend
