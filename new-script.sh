048b50d4b1e12f041e83fba6356cb6254cc76f40
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-20 14:08:51 +0000

    test script

--- new-script.sh
+++ new-script.sh
@@ -1,4 +1,6 @@
 ssh -tt -i /home/jenkins/.ssh/id_rsa ubuntu@52.215.78.226 << EOF
-whoami
-exit
+cd cne-sfia2-brief/database
+sudo docker exec backend bash -c "pytest tests/ --cov application" >> 
+sudo docker exec frontend bash -c "pytest tests/ --cov application"
+sudo docker-compose down
 EOF
