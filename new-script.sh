add15ad4ba7421c2a4606d8baabc905bd8922477
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-20 15:30:39 +0000

    new script

--- new-script.sh
+++ new-script.sh
@@ -10,4 +10,5 @@ sudo docker-compose up -d
 sudo docker exec backend bash -c "pytest tests/ --cov application" 
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
 sudo docker-compose down
+logout
 EOF
