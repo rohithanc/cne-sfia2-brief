b9230a6d7eb514307e63fc15ff8823f1c1afd6ee
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-22 22:16:56 +0000

    Script Update

--- kubernetes/kubernetes.sh
+++ kubernetes/kubernetes.sh
@@ -7,4 +7,5 @@ kubectl apply -f backend-app.yaml && kubectl apply -f backend-service.yaml
 kubectl apply -f frontend-app.yaml && kubectl apply -f frontend-service.yaml
 kubectl get pods -n project
 kubectl get svc -n project
+logout
 EOF
