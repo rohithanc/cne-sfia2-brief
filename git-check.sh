6f0bc686fceb44093c7331ac6e7b4baf09f03b35
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-22 23:23:43 +0000

    remove unnecessary scripts

--- git-check.sh
+++
@@ -1,4 +0,0 @@
-if [ ! -d cne-sfia2-brief ]
-then
-   git clone https://github.com/rohithanc/cne-sfia2-brief
-fi
--- kubernetes-jenkins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash/
-pushd kubernetes
-sudo -H -u ubuntu bash -c 'sudo sh ./kubernetes.sh'
-popd
-
