3874c174a04633b5733221737508e25567eec87a
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-22 17:30:34 +0000

    Kubernetes script

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,9 +1,9 @@
 # Make sure that there is a kubernetes directory
 
-# cd Kubernetes / Name of the Directory
-# kubectl create ns project
-# kubectl apply -f secret.yaml
-# kubectl apply -f backend-app.yaml && kubectl apply -f backend-service.yaml
-# kubectl apply -f frontend-app.yaml && kubectl apply -f frontend-service.yaml
-# kubectl get pods -n project
-# kubectl get svc -n project
+cd Kubernetes / Name of the Directory
+kubectl create ns project
+kubectl apply -f secret.yaml
+kubectl apply -f backend-app.yaml && kubectl apply -f backend-service.yaml
+kubectl apply -f frontend-app.yaml && kubectl apply -f frontend-service.yaml
+kubectl get pods -n project
+kubectl get svc -n project
