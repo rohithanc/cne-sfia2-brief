9c7458267d7681ec1cc6243e7b9f6c8799cd11c2
Ubuntu <ubuntu@ip-172-31-33-188.eu-west-1.compute.internal>
2020-11-23 02:55:51 +0000

    Making sure Docker is downloaded into the PyTest

--- scripts/download-docker-compose.sh
+++ scripts/download-docker-compose.sh
@@ -1,6 +1,10 @@
+#download docker-compose
 sudo apt update
 sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+#download docker
+curl https://get.docker.com | sudo bash
+#download mysql
 sudo apt install mysql-client-core-5.7
